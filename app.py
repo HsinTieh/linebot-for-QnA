6b22c3717e7ba4741294c018083379605de30c6f
cc <03051021@me.mcu.edu.tw>
2019-05-31 22:27:39 +0800

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-#coding:utf-8
-import requests
-from bs4 import BeautifulSoup
-
 from flask import Flask, request, abort
 
 from linebot import (
@@ -26,7 +22,6 @@ def callback():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
-   
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
@@ -38,23 +33,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-     message = TextSendMessage(text=weather_fun(event.message.text))
-     line_bot_api.reply_message(event.reply_token, message)
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
-def weather_fun(message):
-    city=city_fun(message)
-
-    return city
-def city_fun(sentence):
-    if "高雄" in sentence:
-        return 'Kaohsiung_City'
-    elif "台北" in sentence:
-        return 'Taipei_City'    
-    elif "台中" in sentence:
-        return 'Taichung_City'
-    elif "天氣" in sentence:
-         return 'Taichung_City'
-    return sentence
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
