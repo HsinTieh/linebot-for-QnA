128fff9f410f7e6bdd058ade628551fbe2425b18
cc <03051021@me.mcu.edu.tw>
2019-06-01 21:50:48 +0800

    v3

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     pro_qna.columns=['question','answer']
     pro_qna=pro_qna[1:]
     
-	port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
