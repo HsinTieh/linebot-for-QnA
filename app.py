787b6096c3f1eadfb307b5f79fcf2c822b55b1d7
cc <03051021@me.mcu.edu.tw>
2019-06-01 21:54:37 +0800

    v3

--- app.py
+++ app.py
@@ -53,9 +53,6 @@ def searchQuestion():
 
 import os
 if __name__ == "__main__":
-    pro_qna=pd.read_csv('processed.csv',header=None,dtype=str)
-    pro_qna.columns=['question','answer']
-    pro_qna=pro_qna[1:]
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
