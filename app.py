79416471e8134cf07293693996840a2bb262b5b6
cc <03051021@me.mcu.edu.tw>
2019-06-01 17:05:42 +0800

    v3

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=o=processingMssage(event.message.text))
+    message = TextSendMessage(text=processingMssage(event.message.text))
     line_bot_api.reply_message(event.reply_token, message)
 def processingMssage(mes):
     with open('stops.txt', 'r', encoding='utf8') as f:
