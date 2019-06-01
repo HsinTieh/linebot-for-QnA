404d4e68bb4ba038248ccce75cec31d2208a6793
cc <03051021@me.mcu.edu.tw>
2019-06-01 21:57:08 +0800

    v3

--- app.py
+++ app.py
@@ -50,13 +50,14 @@ def processingMssage(mes):
     #searchQuestion(mes_cut)
     return splitedStr
 def searchQuestion():
-    pro_qna=pd.read_csv('processed.csv',header=None,dtype=str)
-    pro_qna.columns=['question','answer']
-    pro_qna=pro_qna[1:]
+
     return 0
 
 import os
 if __name__ == "__main__":
+    pro_qna=pd.read_csv('processed.csv',header=None,dtype=str)
+    pro_qna.columns=['question','answer']
+    pro_qna=pro_qna[1:]
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
